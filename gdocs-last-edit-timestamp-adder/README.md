e3bf1d2a394a1ac8f15e15db60c49080d776e609
Anderson <34724755+andersonaddo@users.noreply.github.com>
2023-03-20 11:08:38 -0400

    Initial upload of new gdocs userscript

---
+++ gdocs-last-edit-timestamp-adder/README.md
@@ -0,0 +1,7 @@
+In March 2023, Google transitioned to Material Design v3 for Google Docs, Slides, and Sheets.
+
+Amongst many things, this change also removed the "Last edit was ______ ago" text in the menubar.
+
+This userscript is an improved version of this [one](https://gist.github.com/ZachTheDev/0df884f490ec4a171ff0a8f8403a7926) (from [here](https://www.reddit.com/r/googledocs/comments/11miktw/want_back_the_last_edit_was_text_install_this/)) that works in Firefox.
+
+Not tested on Chrome.
---
+++ gdocs-last-edit-timestamp-adder/index.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Restore Revision Time Visual Text in Google Apps
+// @version      1.0
+// @description  Brings back visual last edit text in drive apps due to M3 migration by Google.
+// @author       ZachTheDev and andersonaddo
+// @match        https://docs.google.com/document*
+// @match        https://docs.google.com/presentation*
+// @match        https://docs.google.com/spreadsheets*
+// ==/UserScript==
+
+(function () {
+    //Empty just to make sure webhooks for GreasyFork is working
+  })();
