592f60da056e9899b959f46e4087a25cf0559eb9
Anderson <34724755+andersonaddo@users.noreply.github.com>
2023-09-24 11:50:39 -0400

    Improved twitter -> nitter script

--- twitter-redirector/index.js
+++ twitter-redirector/index.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Twitter Login Blocker to Nitter
 // @namespace    happyviking
-// @version      1.0
+// @version      2.0
 // @description  Converts Twitter login flow redirects to Nitter links, and replaces old url in browser history. Also works in Firefox Android.
 // @author       HappyViking
-// @match	     *://*.twitter.com/i/flow/login*
+// @match	     *://*.twitter.com/*
 // @grant        none
 // @run-at       document-start
 // @license      MIT
 // ==/UserScript==
 
 function isProperTargetPage(url) {
-    return !!url.match(/^(|http(s?):\/\/)(.*\.)?twitter.com(\/.*|$)/gim);
+    return !!url.match(/^(|http(s?):\/\/)(.*\.)?twitter.com\/i\/flow\/login(.*|$)/gim);
 }
 
 function getNewUrl() {
@@ -20,7 +20,21 @@ function getNewUrl() {
     return `https://farside.link/nitter/${name ?? ""}`
 }
 
-if (isProperTargetPage(window.location.href)) {
+
+const main = () => {
+    if (isProperTargetPage(window.location.href)) {
     const newUrl = getNewUrl()
     location.replace(newUrl);
 }
+}
+
+//There are probably cleaner ways to do this but I don't really care, this works and is simple
+let currentPage = location.href;
+main()
+setInterval(() =>
+{
+    if (currentPage != location.href){
+        currentPage = location.href;
+        main()
+    }
+}, 500);
